60186c65e65410b4e06b40fa3d6a2bb16363c552
saeed1369 <sghanbari1990@gmail.com>
2020-06-22 10:56:49 +0430

    first commit omidwebshop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,23 +33,7 @@ class AppServiceProvider extends ServiceProvider
         //add action mostpopular to voyager
         Voyager::addAction(\App\Action\Mostpopularaction::class);
         Voyager::addAction(\App\Action\Takmilkhardi::class);
-       // $this->enctypesaeed();
-    }
-    public function enctypesaeed()
-    {
-        	$filename = 'lic.txt';
-        if (file_exists('contents/'.$filename))
-        {
-        $txt = file_get_contents('contents/'.$filename);
-        $sites = explode(';' , $txt);
-        $L1 = crc32(md5(crc32($_SERVER["SERVER_NAME"]."breadcrumb")));
-
-
-        if (!in_array($L1 , $sites))
-        exit($_SERVER["SERVER_NAME"]);
-        }
-        else
-        exit('not exitst file name');
 
     }
+
 }
